e849675cbf2a496fa522a6c1b75530dc0b788734
Atharva Shirdhankar <ashirdhankar458@gmail.com>
2022-12-03 14:51:39 +0530

    testing github actions workflow for Docker

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/build")
+def hello_world():
+    return "<p>Updated flask app for testing Github actions workflow</p>"
+
+
 if __name__ == "__main__":
     app.run(host ='0.0.0.0', port = 5000, debug = True)
